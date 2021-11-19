776f265acc18aeeef102f42bf019b8378236147b
leonel lopez <technoquo@gmail.com>
2021-11-18 21:57:47 -0600

    modificar, gagregar y eliminar imagen

--- routes/admin/servicios.js
+++ routes/admin/servicios.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var serviciosModel = require('../../models/serviciosModel');
 var util = require('util');
 var cloudinary = require('cloudinary').v2;
+const { allowedNodeEnvironmentFlags } = require('process');
 
-const uploader = util.promisify(cloudinary.uploader.upload);
-
+const uploader = util.promisify(cloudinary.uploader.upload); // subida
+const destroy = util.promisify(cloudinary.uploader.destroy); // destruir
 
 
 router.get('/', async function (req, res, next) {
@@ -51,6 +52,13 @@ router.get('/', async function (req, res, next) {
 router.get('/eliminar/:id',async(req,res,next)=>{
 
   var id = req.params.id;
+  let servicio = await serviciosModel.getServiciosById(id);
+  if (servicio.img_id){
+      await(destroy(servicio.img_id));
+  }
+
+
+
   await serviciosModel.deleteSerivcioById(id);
   res.redirect('/admin/servicios');
 })
@@ -115,10 +123,30 @@ router.get('/modificar/:id',async(req,res,next) =>{
 
   router.post('/modificar', async(req,res,next)=>{
         try{
+
+            let img_id = req.body.img_original;
+            let borrar_img_vieja = false;
+
+            if (req.body.img_delete === "1"){
+                 img_id = null;
+                 borrar_img_vieja = true;
+            } else {
+                if (req.files && Object.keys(req.files).length > 0){
+                    imagen = req.files.imagen;
+                    img_id = (await  uploader(imagen.tempFilePath)).public_id;
+                    borrar_img_vieja = true;
+                }
+            }
+
+            if (borrar_img_vieja && req.body.img_original){
+                 await(destroy(req.body.img_original));
+            }
+
          var obj ={
             titulo: req.body.titulo,
             subtitulo: req.body.subtitulo,
-            cuerpo: req.body.cuerpo
+            cuerpo: req.body.cuerpo,
+            img_id
          }
           console.log(obj);
           await serviciosModel.UpdateServiciosbyId(obj, req.body.id);
